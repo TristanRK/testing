2e920bf3c81690fd2e4ce11f64b1308a9ae10174
TristanRK <tristankrafft08@gmail.com>
2021-07-29 11:17:09 +0200

    Asking a question

--- TestingR.R
+++ TestingR.R
@@ -1,3 +1,5 @@
 print("this file was created within RStudio")
 print("and now it lives on github")
 print("and this will be where i test the stageing, committing and pushing in RStudio")
+
+print("what did the fox say?")
